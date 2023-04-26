6dcf171d2c781f1ee096e7b51f9b402c7812111b
Ammar Hardan <Ammar.Hardan@iHorizons.com>
2023-04-26 21:39:00 +0300

    set up project

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require("gulp-sourcemaps");
+
+gulp.task("sass", function(done) {
+  return gulp
+    .src("./src/sass/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./dist/css"));
+  done();
+});
